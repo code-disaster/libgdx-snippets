19d829d9e8393dc6d0e02c98ae87333eaa540983
Daniel Ludwig <codi@code-disaster.com>
2015-07-11 21:52:10 +0200

    Fixed library names.

--- src/main/java/com/badlogic/gdx/utils/GdxSnippetsNativesLoader.java
+++ src/main/java/com/badlogic/gdx/utils/GdxSnippetsNativesLoader.java
@@ -5,11 +5,32 @@ public class GdxSnippetsNativesLoader {
 	/**
 	 * Loads the native library, and initializes the flextGL OpenGL bindings.
 	 */
-	public static synchronized void load() {
+	public static synchronized void load(boolean setupGLBindings) {
 
 		try {
-			new SharedLibraryLoader().load("gdx-snippets");
-			flextInit();
+
+			// customized library name: 32/64 bit shared library on OS X
+
+			SharedLibraryLoader loader = new SharedLibraryLoader() {
+				@Override
+				public String mapLibraryName(String libraryName) {
+					if (isMac) {
+						return "lib" + libraryName + ".dylib";
+					}
+					return super.mapLibraryName(libraryName);
+				}
+			};
+
+			// load native library
+
+			loader.load("gdx-snippets");
+
+			// initialize flextGL bindings
+
+			if (setupGLBindings) {
+				flextInit();
+			}
+
 		} catch (Exception e) {
 			System.err.println(e.getMessage());
 		}
