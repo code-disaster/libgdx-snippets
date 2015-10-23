bf101a7215e7dbbf5ccb51153adb5f8bc116921c
Daniel Ludwig <codi@code-disaster.com>
2015-10-23 11:16:14 +0200

    Added functional wrappers for Reentrant[ReadWrite]Lock.

---
+++ src/main/java/com/badlogic/gdx/concurrent/ReentrantLock.java
@@ -0,0 +1,54 @@
+package com.badlogic.gdx.concurrent;
+
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.Lock;
+import java.util.function.Supplier;
+
+/**
+ * A functional style wrapper to {@link java.util.concurrent.locks.ReentrantLock}.
+ */
+public class ReentrantLock {
+
+	private final Lock lock = new java.util.concurrent.locks.ReentrantLock();
+
+	public void lock(Runnable runnable) {
+		try {
+			lock.lock();
+			runnable.run();
+		} finally {
+			lock.unlock();
+		}
+	}
+
+	public <T> T lock(Supplier<T> supplier) {
+		try {
+			lock.lock();
+			return supplier.get();
+		} finally {
+			lock.unlock();
+		}
+	}
+
+	public <T> T tryLock(Supplier<T> supplier, T defaultValue) {
+		if (!lock.tryLock()) {
+			return defaultValue;
+		}
+		try {
+			return supplier.get();
+		} finally {
+			lock.unlock();
+		}
+	}
+
+	public <T> T tryLock(Supplier<T> supplier, T defaultValue, long time, TimeUnit unit) throws InterruptedException {
+		if (!lock.tryLock(time, unit)) {
+			return defaultValue;
+		}
+		try {
+			return supplier.get();
+		} finally {
+			lock.unlock();
+		}
+	}
+
+}
---
+++ src/main/java/com/badlogic/gdx/concurrent/ReentrantReadWriteLock.java
@@ -0,0 +1,94 @@
+package com.badlogic.gdx.concurrent;
+
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.function.Supplier;
+
+/**
+ * A functional style wrapper to {@link java.util.concurrent.locks.ReentrantReadWriteLock}.
+ */
+public class ReentrantReadWriteLock {
+
+	private final ReadWriteLock lock = new java.util.concurrent.locks.ReentrantReadWriteLock();
+
+	public void read(Runnable runnable) {
+		try {
+			lock.readLock().lock();
+			runnable.run();
+		} finally {
+			lock.readLock().unlock();
+		}
+	}
+
+	public <T> T read(Supplier<T> supplier) {
+		try {
+			lock.readLock().lock();
+			return supplier.get();
+		} finally {
+			lock.readLock().unlock();
+		}
+	}
+
+	public <T> T tryRead(Supplier<T> supplier, T defaultValue) {
+		if (!lock.readLock().tryLock()) {
+			return defaultValue;
+		}
+		try {
+			return supplier.get();
+		} finally {
+			lock.readLock().unlock();
+		}
+	}
+
+	public <T> T tryRead(Supplier<T> supplier, T defaultValue, long time, TimeUnit unit) throws InterruptedException {
+		if (!lock.readLock().tryLock(time, unit)) {
+			return defaultValue;
+		}
+		try {
+			return supplier.get();
+		} finally {
+			lock.readLock().unlock();
+		}
+	}
+
+	public void write(Runnable runnable) {
+		try {
+			lock.writeLock().lock();
+			runnable.run();
+		} finally {
+			lock.writeLock().unlock();
+		}
+	}
+
+	public <T> T write(Supplier<T> supplier) {
+		try {
+			lock.writeLock().lock();
+			return supplier.get();
+		} finally {
+			lock.writeLock().unlock();
+		}
+	}
+
+	public <T> T tryWrite(Supplier<T> supplier, T defaultValue) {
+		if (!lock.writeLock().tryLock()) {
+			return defaultValue;
+		}
+		try {
+			return supplier.get();
+		} finally {
+			lock.writeLock().unlock();
+		}
+	}
+
+	public <T> T tryWrite(Supplier<T> supplier, T defaultValue, long time, TimeUnit unit) throws InterruptedException {
+		if (!lock.writeLock().tryLock(time, unit)) {
+			return defaultValue;
+		}
+		try {
+			return supplier.get();
+		} finally {
+			lock.writeLock().unlock();
+		}
+	}
+
+}
