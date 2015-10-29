4cfc9ad431e5241ed18cc91649706b834c165066
Daniel Ludwig <codi@code-disaster.com>
2015-10-29 18:54:53 +0100

    Added ThreadLocalArray, ThrowableSupplier.

---
+++ src/main/java/com/badlogic/gdx/concurrent/ThreadLocalArray.java
@@ -0,0 +1,50 @@
+package com.badlogic.gdx.concurrent;
+
+import com.badlogic.gdx.function.ThrowableSupplier;
+import com.badlogic.gdx.utils.GdxRuntimeException;
+import com.badlogic.gdx.utils.reflect.*;
+
+import java.util.function.Supplier;
+
+/**
+ * A convenience wrapper to {@link ThreadLocal} storing an array of objects.
+ *
+ * <pre>
+ * {@code
+ * ThreadLocalArray<UserObject> tls = new ThreadLocalArray<>(64, UserObject.class, () -> new UserObject(params...));
+ * UserObject[] array = tls.get();
+ * }
+ * </pre>
+ */
+public class ThreadLocalArray<T> implements Supplier<T[]> {
+
+	private final ThreadLocal<T[]> tls = new ThreadLocal<>();
+
+	public ThreadLocalArray(int capacity, Class<? extends T> clazz) {
+		this(capacity, clazz, () -> ClassReflection.newInstance(clazz));
+	}
+
+	@SuppressWarnings("unchecked")
+	public ThreadLocalArray(int capacity, Class<? extends T> clazz,
+							ThrowableSupplier<T, ReflectionException> initialValueSupplier) {
+
+		try {
+
+			T[] values = (T[]) ArrayReflection.newInstance(clazz, capacity);
+
+			for (int i = 0; i < capacity; i++) {
+				values[i] = initialValueSupplier.get();
+			}
+
+			tls.set(values);
+
+		} catch (ReflectionException e) {
+			throw new GdxRuntimeException(e);
+		}
+	}
+
+	@Override
+	public T[] get() {
+		return tls.get();
+	}
+}
---
+++ src/main/java/com/badlogic/gdx/function/ThrowableSupplier.java
@@ -0,0 +1,12 @@
+package com.badlogic.gdx.function;
+
+/**
+ * A variant of {@link java.util.function.Supplier} which can throw an exception.
+ *
+ * @param <T> the type of result supplied by this supplier
+ * @param <E> the type of exception to be handled
+ */
+@FunctionalInterface
+public interface ThrowableSupplier<T, E extends Throwable> {
+	T get() throws E;
+}
