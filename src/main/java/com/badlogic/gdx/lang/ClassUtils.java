ecaca4b0fe56d96ac6650705530182da3bc48924
Daniel Ludwig <codi@code-disaster.com>
2016-10-11 10:20:15 +0200

    Added utility function to hash .class resources.

--- src/main/java/com/badlogic/gdx/lang/ClassUtils.java
+++ src/main/java/com/badlogic/gdx/lang/ClassUtils.java
@@ -1,7 +1,11 @@
 package com.badlogic.gdx.lang;
 
+import com.badlogic.gdx.checksum.SHA1;
+import com.badlogic.gdx.utils.FileUtils;
 import com.badlogic.gdx.utils.GdxRuntimeException;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.lang.reflect.ParameterizedType;
 import java.lang.reflect.Type;
 
@@ -28,6 +32,30 @@ public class ClassUtils {
 			throw new GdxRuntimeException(e);
 		}
 
+	}
+
+	/**
+	 * Calculates the SHA-1 hash on the byte code (the .class file content) of the given {@link Class}.
+	 */
+	public static SHA1 getClassHash(Class<?> classType) throws IOException {
+
+		String resourcePath = "/" + classType.getName().replace('.', '/') + ".class";
+		InputStream resource = classType.getResourceAsStream(resourcePath);
+
+		return FileUtils.hashStream(resource);
+	}
+
+	/**
+	 * Calculates the SHA-1 hash on the byte code (the .class file content) of the given {@link Class}.
+	 *
+	 * This version returns {@code defaultHash} if an {@link IOException} is thrown.
+	 */
+	public static SHA1 getClassHash(Class<?> classType, SHA1 defaultHash) {
+		try {
+			return getClassHash(classType);
+		} catch (IOException e) {
+			return defaultHash;
+		}
 	}
 
 }
