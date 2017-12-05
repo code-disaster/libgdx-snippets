74755db0060e2168e84c6313c7025755fe0ae2a4
Daniel Ludwig <codi@code-disaster.com>
2017-12-05 13:22:26 +0100

    Removed use of sun.misc.Unsafe.

--- src/main/java/com/badlogic/gdx/utils/Memory.java
+++ src/main/java/com/badlogic/gdx/utils/Memory.java
@@ -1,25 +1,16 @@
 package com.badlogic.gdx.utils;
 
-import sun.misc.Unsafe;
-
-import java.lang.reflect.Field;
-import java.nio.Buffer;
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
 
 /**
  * Memory utility functions.
- *
- * Uses sun.misc.Unsafe.
  */
 public class Memory {
 
-	private static final Unsafe theUnsafe;
-	private static final long addressFieldOffset;
-
 	/**
 	 * Allocates a direct {@link ByteBuffer} with an user-defined alignment.
-	 *
+	 * <p>
 	 * Internally a slightly larger buffer is created, wasting up to [alignment] bytes of native memory.
 	 */
 	public static ByteBuffer allocateDirectAligned(int capacity, long alignment) {
@@ -45,7 +36,7 @@ public class Memory {
 
 	/**
 	 * Uses the native memset() function to clear parts of a direct {@link ByteBuffer}.
-	 *
+	 * <p>
 	 * Does neither observe nor modify {@link ByteBuffer#position()} and {@link ByteBuffer#limit()}, which means
 	 * the state of the ByteBuffer object is not changed by this function.
 	 */
@@ -67,32 +58,17 @@ public class Memory {
 	}
 
 	public static long getAddress(ByteBuffer buffer) {
-		return theUnsafe.getLong(buffer, addressFieldOffset);
-	}
-
-	private static Unsafe getUnsafe() {
-		try {
-			Field f = Unsafe.class.getDeclaredField("theUnsafe");
-			f.setAccessible(true);
-			return (Unsafe)f.get(null);
-		} catch (Exception e) {
-			throw new RuntimeException(e);
-		}
-	}
-
-	static {
-		try {
-			theUnsafe = getUnsafe();
-			addressFieldOffset = theUnsafe.objectFieldOffset(Buffer.class.getDeclaredField("address"));
-		} catch (NoSuchFieldException e) {
-			throw new RuntimeException(e);
-		}
+		return getAddress(buffer, 0);
 	}
 
 	// @off
 
 	/*JNI
 		#include <memory.h>
+	*/
+
+	private static native long getAddress(ByteBuffer buffer, int offset); /*
+		return (jlong) &buffer[offset];
 	*/
 
 	private static native void memset(long address, int value, int offset, int count); /*
